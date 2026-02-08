32fe490e4a067e0e6854f099157ee268fa2e61d0
Loic Leforestier <loicleforestier.pro@gmail.com>
2026-02-08 18:45:51 +0100

    feat player controller: added camera movement + level

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -1,11 +1,42 @@
-extends Node
+extends CharacterBody3D
 
+@export_group("Camera")
+@export_range(0.0, 1.0) var mouse_sensitivity := .25
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export_group("Movement")
+@export var move_speed := 8.0
+@export var acceleration := 20.0
 
+@onready var _camera_pivot: Node3D = %CameraPivot
+@onready var _camera: Node3D = %Camera
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var _camera_input_direction := Vector2.ZERO
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var mouse_event = event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion := (
+		event is InputEventMouseMotion and
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion:
+		var event_mouse_motion = event as InputEventMouseMotion
+		_camera_input_direction = event_mouse_motion.screen_relative * mouse_sensitivity
+
+func _physics_process(delta: float) -> void:
+	_camera_pivot.rotation.x += _camera_input_direction.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, deg_to_rad(0), deg_to_rad(80))
+	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+	
+	_camera_input_direction = Vector2.ZERO
+	
+	var raw_input := Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
+	var forward = _camera.global_basis.z
+	var right = _camera.global_basis.x
