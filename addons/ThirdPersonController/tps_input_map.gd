273db649f642be2bee093c63413d62a4884255e0
Safirl <loic.leforestier@mmibordeaux.com>
2026-02-10 22:08:22 +0100

    added input dynamic map

--- addons/ThirdPersonController/tps_input_map.gd
+++ addons/ThirdPersonController/tps_input_map.gd
@@ -1,18 +1,28 @@
 extends Node
 
+class TPSInput:
+	var action_name: String
+	var events: Array[InputEvent]
 
-# Called when the node enters the scene tree for the first time.
+var inputs: Array[TPSInput]
+
 func _ready() -> void:
-	if not InputMap:
+	if InputMap == null:
+		push_error("Input map is null")
 		return;
-	var ev = InputEventAction.new()
-	# Set as ui_left, pressed.
-	ev.action = "ui_left"
-	ev.pressed = true
-	# Feedback.
-	Input.parse_input_event(ev)
+		
+	_create_input_events()
+	_register_inputs()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+## Manually add inputs here. They will be automatically added to the InputMap
+func _create_input_events() -> void:
 	pass
+
+func _register_inputs() -> void:
+	for input in inputs:
+		if not InputMap.has_action(input.action_name):
+			InputMap.add_action(input.action_name)
+		
+		for input_event in input.events:
+			if not InputMap.action_has_event(input.action_name, input_event):
+				InputMap.action_add_event(input.action_name, input_event)
