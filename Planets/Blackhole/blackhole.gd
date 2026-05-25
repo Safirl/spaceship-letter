99c890300ff052e7d9813db373f30424f62a0cde
Loic Leforestier <loicleforestier.pro@gmail.com>
2026-05-25 17:38:20 +0200

    removed dead code

--- Planets/Blackhole/blackhole.gd
+++ Planets/Blackhole/blackhole.gd
@@ -1,23 +1,23 @@
 extends Node3D
 
 @onready var meshInstance: MeshInstance3D = %Mesh
-@export var blackhole_camera: Camera3D
+@export var player_camera: Camera3D
 @export var blackhole_subviewport: SubViewport
 var blackhole_material: ShaderMaterial
 
 func _ready() -> void:
-	if (!blackhole_camera):
-		blackhole_camera = get_tree().current_scene.find_child("BlackholeCamera")
+	if (!player_camera):
+		player_camera = get_tree().current_scene.find_child("PlayerCamera")
 		
-	if (!blackhole_subviewport):
-		blackhole_subviewport = get_tree().current_scene.find_child("BlackholeViewport")
-	
-	blackhole_material = meshInstance.mesh.material as ShaderMaterial
-	if (!blackhole_material):
-		push_error("ShaderMaterial not valid ! Can't initialize shader")
+	#if (!blackhole_subviewport):
+		#blackhole_subviewport = get_tree().current_scene.find_child("BlackholeViewport")
+	#
+	#blackhole_material = meshInstance.mesh.material as ShaderMaterial
+	#if (!blackhole_material):
+		#push_error("ShaderMaterial not valid ! Can't initialize shader")
 	#blackhole_material.set_shader_parameter("screen_texture", blackhole_subviewport.get_texture())
 
 
 
 func _process(delta: float) -> void:
-	look_at(blackhole_camera.global_position)
+	look_at(player_camera.global_position)
